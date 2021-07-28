30da68a5785f07663168b765fe7cb1e17d624c74
Serhii <43697311+SerhiiSpas@users.noreply.github.com>
2021-07-28 10:16:23 +0300

    ver 1.1

--- HW_17/Program.cs
+++ HW_17/Program.cs
@@ -4,124 +4,66 @@ using System.Linq;
 
 namespace HW_17
 {
-    class Program
+    class welcome
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter student's name");
-            Student name = new Student(Console.ReadLine());
+  
+            Console.WriteLine("Program for tracking student progress in C#" + "\n");
 
-            s1:
-            Console.WriteLine("Enter grade in Math 1 to 12");
-            int ballMath = int.Parse(Console.ReadLine());
-            if (ballMath > 12 || ballMath <= 0)
-            {
-                Console.Write("You enter not correct grade. Try again" + "\n");
-                goto s1;
-            }
+            Console.WriteLine("Enter student name\r");
+            string name = Console.ReadLine();
 
-            s2:
-            Console.WriteLine("Enter grade in Literature 1 to 12");
-            int ballliterature = int.Parse(Console.ReadLine());
-            if (ballliterature > 12 || ballliterature <= 0)
-            {
-                Console.Write("You enter not correct grade. Try again" + "\n");
-                goto s2;
-            }
-
-            s3:
-            Console.WriteLine("Enter grade in Physicse 1 to 12");
-            int ballphysics = int.Parse(Console.ReadLine());
-            if (ballphysics > 12 || ballphysics <= 0)
-            {
-                Console.Write("You enter not correct grade. Try again" + "\n");
-                goto s3;
-            }
-
-            s4:
-            Console.WriteLine("Enter grade in Physical class 1 to 12");
-            int ballPhysicalT = int.Parse(Console.ReadLine());
-            if (ballPhysicalT > 12 || ballPhysicalT <= 0)
-            {
-                Console.Write("You enter not correct grade. Try again" + "\n");
-                goto s4;
-            }
-
-            name.Diary.AddMark(ballMath);
-            name.Diary.AddMark(ballliterature);
-            name.Diary.AddMark(ballphysics);
-            name.Diary.AddMark(ballPhysicalT);
+            Console.WriteLine("Hello " + name + "! Choose an option from the following list\r");
             
+            Console.WriteLine("\ta - Add");
+            /*Console.WriteLine("\ts - ");
+            Console.WriteLine("\tm - ");
+            Console.WriteLine("\td - ");*/
+            Console.Write("Your option? ");
 
-            Console.WriteLine($"Max mark:\t{name.Diary.MaxMark}");
-            Console.WriteLine($"Min mark:\t{name.Diary.MinMark}");
-            Console.WriteLine($"Average mark:\t{name.Diary.AverageMark}");
-        }
-    }
-
-    public class Diary
-    {
-        private readonly IList<int> marks;
-
-        public int this[int index]
-        {
-            get
+            string enter = Console.ReadLine();
+            if (enter == "a")
             {
-                if (index >= 0 && index < marks.Count)
+                Console.Write("Enter the number of grade to be added:\t");
+                int elementsGrade = int.Parse(Console.ReadLine());
+                int[] ballMath = new int[elementsGrade];
+
+                Console.WriteLine("Enter grade in Math\r");
+
+                for (int i = 0; i < ballMath.Length; i++)
                 {
-                    return marks[index];
+                    Console.WriteLine($"Enter the grade under the index number {i}:\r");
+                    ballMath[i] = int.Parse(Console.ReadLine());
                 }
 
-                throw new IndexOutOfRangeException();
             }
-            // На случай ошибочно выставленной оценки
-            set
-            {
-                if (index >= 0 && index < marks.Count)
-                {
-                    marks[index] = value;
-                }
-
-                throw new IndexOutOfRangeException();
-            }
+            
         }
-
-        public Diary()
-        {
-            marks = new List<int>();
-        }
-
-        public void AddMark(int mark)
-        {
-            // Проверка корректности оценки (по 5-ти бальной системе)
-            if (mark > 0 && mark <= 12)
-            {
-                this.marks.Add(mark);
-                return;
-            }
-
-            throw new IndexOutOfRangeException(nameof(mark));
-        }
-
-        public int MaxMark => marks.Max();
-
-        public int MinMark => marks.Min();
-
-        public double AverageMark => marks.Average();
     }
 
-    public class Student
+    /*class Add
     {
-        public string Name { get; set; }
-
-        public Diary Diary { get; private set; }
-
-        public Student(string name)
+        static void Main1(string[] args)
         {
-            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException(nameof(name));
 
-            Name = name;
-            Diary = new Diary();
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine("Your result: 1" );
+                    break;
+                case "s":
+                    Console.WriteLine("Your result: 2");
+                    break;
+                case "m":
+                    Console.WriteLine("Your result: 3");
+                    break;
+                case "d":
+                    Console.WriteLine("Your result: 4");
+                    break;
+            }
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
         }
-    }
+    }*/
 }
